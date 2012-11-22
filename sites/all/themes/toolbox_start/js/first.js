91f4bb2b8f118b047b4bb63da5b145735a6bfc68
Alin <tincasalin@yahoo.com>
2012-11-22 13:17:02 +0100

    topics style

--- sites/all/themes/toolbox_start/js/first.js
+++ sites/all/themes/toolbox_start/js/first.js
@@ -11,4 +11,13 @@ Drupal.behaviors.myBehavior = {
 if ($("#forum .icon div").hasClass("topic-status-new"))  {
      ($("#forum tbody tr:has(.topic-status-new)").addClass("go_bold"));
  }     
+ 
+//if ($("article header p").hasClass("submitted_header")) {
+ //       ($("article header:has(.submitted_header)").hide());}
+
+//$("#main #content #page-title").prependTo("article:has(.field-name-taxonomy-forums) header");
+
+$("article header .submitted .permalink").hide();
+$("article .field-name-taxonomy-forums").hide();
+//$("article header .submitted").appendTo("article header h3");
   }}})(jQuery);

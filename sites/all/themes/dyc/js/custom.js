76b31609e69835139e26545d9a03fbaf8b456472
Kreativt Team <kreativt-team@cyberhus.dk>
2013-02-07 14:40:22 +0100

    standartize spacing and delimitation

--- sites/all/themes/dyc/js/custom.js
+++ sites/all/themes/dyc/js/custom.js
@@ -1,28 +1,29 @@
 (function ($) {
-	//add drupal 7 code
-	Drupal.behaviors.myfunction = {
-	        attach: function(context, settings) {
-	//end drupal calls
-			// Hide all answers on the FAQ page
-			$('.answer').hide();
+  //add drupal 7 code
+  Drupal.behaviors.myfunction = {
+    attach: function(context, settings) {
+  //end drupal calls
+      
+// Hide all answers on the FAQ page
+      $('.answer').hide();
 
-			// Show each answer when it's question is clicked and hide it back again 
-			$('.question').toggle(
-			function(){
-				$(this).parent().addClass('active');
-				$(this).parent().children('.answer').slideDown();
-			},
-			function(){
-				$(this).parent().children('.answer').slideUp();
-				$(this).parent().removeClass('active');
-			});
+// Show each answer when it's question is clicked and hide it back again 
+      $('.question').toggle(
+        function(){
+          $(this).parent().addClass('active');
+            $(this).parent().children('.answer').slideDown();
+        },
+        function(){
+          $(this).parent().children('.answer').slideUp();
+            $(this).parent().removeClass('active');
+      });
 
-			// Add active class to list items in the recent article block to help style the current article
+// Add active class to list items in the recent article block to help style the current article
 
-			$('.view-display-id-block_1 li a.active').parents('li').addClass('active');
+      $('.view-display-id-block_1 li a.active').parents('li').addClass('active');
 
-			// Inject btn class to add new forum topic on the forum page
-			$('.page-forum #zone2 ul.links li a').addClass('btn btn-theme btn-large');
-		}
-	}
+// Inject btn class to add new forum topic on the forum page
+      $('.page-forum #zone2 ul.links li a').addClass('btn btn-theme btn-large');
+    }
+  }
 })(jQuery);
